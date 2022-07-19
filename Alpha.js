b46cc7bcfa7d0c4c6a5260a0b64d2425e52908fb
HAZI <84738395+Isuru22@users.noreply.github.com>
2022-07-19 12:26:28 +0530

    Update Alpha.js

--- Alpha.js
+++ Alpha.js
@@ -3859,7 +3859,7 @@ break
  
 ||Thank for use bot```
  
-  ------------------------`,
+  ------------------------/n/n`,
                     footer: GojoMdNx.user.name,
                     buttons: buttons,
                     headerType: 4
